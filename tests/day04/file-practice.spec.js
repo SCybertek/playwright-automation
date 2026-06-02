410fd22c12c91101daed3ad8a02ade60d7b24c08
SCybertek <snuryyeva@gmail.com>
2026-06-01 20:05:47 -0400

    removed unused test blocks

--- tests/day04/file-practice.spec.js
+++ tests/day04/file-practice.spec.js
@@ -2,8 +2,4 @@ import { test } from "@playwright/test";
 
 test.describe("", () => {
   test("", async ({ page }) => {});
-
-  test("", async ({ page }) => {});
-
-  test("", async ({ page }) => {});
 });
